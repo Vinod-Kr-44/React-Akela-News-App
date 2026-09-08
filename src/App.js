8d4fa98c0b7201eb91d73e78dfdfd87a20214d2b
Vinod Kumar <vkpal@ictrlbiz.com>
2026-09-08 11:39:47 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -12,15 +12,24 @@ export default class App extends Component {
   render() {
     return (
        <BrowserRouter>
+
         <div>
           <NavBar/>
-          {/* <News/> */}
         </div>
+
         <Routes>
-          <Route path="/" element={<News />} />
+          <Route path="/" element={<News key='general' country='us' pageSize='4'  category='general'/>} />
+          <Route path="/business" element={<News key='business' country='us' pageSize='4'  category='business'/>} />
+          <Route path="/entertainment" element={<News key='entertainment' country='us' pageSize='4'  category='entertainment'/>} />
+          <Route path="/general" element={<News key='general' country='us' pageSize='4'  category='general'/>} />
+          <Route path="/health" element={<News key='health' country='us' pageSize='4'  category='health'/>} />
+          <Route path="/science" element={<News key='science' country='us' pageSize='4'  category='science'/>} />
+          <Route path="/sports" element={<News key='sports' country='us' pageSize='4'  category='sports'/>} />
+          <Route path="/technology" element={<News key='technology' country='us' pageSize='4'  category='technology'/>} />
           <Route path="/about" element={<About />}/>
         </Routes>
       </BrowserRouter>
     )
   }
 }
+
--- src/components/News.js
+++ src/components/News.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+
 
 export class News extends Component {
    
+     static defaultProps={
+        country:'in',
+        pageSize:5,
+        category:'health'
+    };
+
+    PropTypes={
+        country:PropTypes.string,
+        pageSize:PropTypes.string,
+        category:PropTypes.string
+    }
+
     constructor() {
         super();
         this.state={
             articles:[],
-            loading:false
+            loading:false,
+            page:1
         }
     }
 
     async componentDidMount(){
-        let url='https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3a3e211534014c0caf7eca95828ef9ab';
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a3e211534014c0caf7eca95828ef9ab&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true})
         let data =await fetch(url);
         let parseData=await data.json();
         console.log(parseData);
-        this.setState({articles:parseData.articles});
+        this.setState({articles:parseData.articles,totalResults:parseData.totalResults,loading:false});
     }
+
+    handlePrevious = async ()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a3e211534014c0caf7eca95828ef9ab&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+         this.setState({loading:true})
+        let data =await fetch(url);
+        let parseData=await data.json();
+        console.log(parseData);
+        this.setState({
+            articles:parseData.articles,
+            page:this.state.page-1,
+            loading:false
+        });
+    }
+    handleNext= async ()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a3e211534014c0caf7eca95828ef9ab&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+         this.setState({loading:true})
+        let data =await fetch(url);
+        let parseData=await data.json();
+        console.log(parseData);
+        this.setState({
+            articles:parseData.articles,
+            page:this.state.page+1,
+            loading:false
+        });
+    }
+   
 
   render() {
     return (
       <div className='container my-3'>
-        <h3>Top Headlines</h3>
+        <h3 className='text-center'>Akela News - Top Headlines</h3>
+        {this.state.loading && <Spinner/>}
         <div className='row'>
             {this.state.articles.map((element)=>{
                 return  <div className='col-md-3' key={element.url} >
@@ -30,6 +74,11 @@ export class News extends Component {
                         </div>
             })}
            
+        </div>
+
+        <div className='container d-flex justify-content-between my-3'>
+            <button disabled={this.state.page<=1} type="button" className='btn btn-dark' onClick={this.handlePrevious} >&larr; Previuos</button>
+            <button type="button" className='btn btn-dark' onClick={this.handleNext}>Next &rarr;</button>
         </div>
         
       </div>
